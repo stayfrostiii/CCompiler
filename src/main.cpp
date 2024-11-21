23010da1043a38825b99f5c8a1bc2126d8b3f7b6
stayfrostiii <jowu.32@gmail.com>
2024-11-20 19:13:13 -0800

    better parsing

--- src/main.cpp
+++ src/main.cpp
@@ -38,12 +38,14 @@ int main()
         "true", "false"
     };
 
-
-    string filename = "..\\..\\..\\testFiles\\add.txt";
+    string filename = "..\\..\\..\\testFiles\\Test.java";
     ifstream javaFile;
     javaFile.open(filename);
-    vector<string> lines;
+    vector<string> object;
+    string fileString;
     string line;
+
+    fileString = "";
 
     if (!javaFile.is_open()) {
         cerr << "Error: Could not open the file " << filename << endl;
@@ -52,16 +54,46 @@ int main()
         return 1;
     }
     
-    while (getline(javaFile, line))
+    string singleComm = "//";
+
+    // while (getline(javaFile, thing))
+    // {
+
+    //     if (line.substr(0,2) != singleComm)
+    //     {
+    //         fileString += line;
+    //         lines.push_back(line); 
+    //     }
+    // } 
+
+    char ch;
+
+    while (javaFile.get(ch))
     {
-        lines.push_back(line);
-    } 
+        if (ch == '\n' || ch == ' ' || ch =='\t' || ch == '\r' || ch == '\f' || 
+            ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == ';')
+        {
+            if (ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == ';')
+            {
+                object.push_back(line);
+                object.push_back(string(1, ch));
+                line = "";
+            }
+            else if (line != "")
+            {
+                object.push_back(line);
+                line = "";
+            }
+        }
+        else
+            line += ch;
+    }
 
     javaFile.close();
 
-    for (int i = 0; i < lines.size(); i++)
+    for (int i = 0; i < object.size(); i++)
     {
-        cout << i + 1 << ". " << lines.at(i) << endl;
+        cout << i + 1 << ". " << object.at(i) << endl;
     }
 
     cout << "Press any key to continue...";
--- testFiles/Test.java
+++ testFiles/Test.java
@@ -1,4 +1,7 @@
-public class Test { 
+
+// Comment test
+
+public class Test {
     public static void main(String args[]) 
     { 
         int a = 5;
